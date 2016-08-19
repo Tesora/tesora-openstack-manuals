751d80d1e6f271765bcd7480aadb637988efd1cc
Brian Hunter <bhunter@tesora.com>
2016-08-19 13:38:20 -0400

    tweak cli reference
    builds tesora-trove.html, that's an index followed by all the
    commands
    
    Change-Id: If5aa574846e9bfdae2f96d22dde186cda29d7086

--- tools/build-all-rst.sh
+++ tools/build-all-rst.sh
@@ -9,9 +9,10 @@ if [[ $# > 0 ]] ; then
     fi
 fi
 
-# Do not build from stable/mitaka
-for guide in user-guide admin-guide \
-    contributor-guide image-guide arch-design cli-reference; do
+# possible values:
+# user-guide admin-guide contributor-guide image-guide arch-design cli-reference; do
+
+for guide in cli-reference; do
     tools/build-rst.sh doc/$guide --build build \
         --target $guide $LINKCHECK
 done
@@ -19,9 +20,9 @@ done
 # Draft guides
 # This includes guides that we publish from stable branches
 # as versioned like the networking-guide.
-for guide in networking-guide config-reference ; do
-    tools/build-rst.sh doc/$guide --build build \
-        --target "mitaka/$guide" $LINKCHECK
-done
+#for guide in networking-guide config-reference ; do
+#    tools/build-rst.sh doc/$guide --build build \
+#        --target "mitaka/$guide" $LINKCHECK
+#done
 
 tools/build-install-guides-rst.sh $LINKCHECK
